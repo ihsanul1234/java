02a42829fdd5de5d6768120125201d17031de569
Ihsanul Haq <131159276+ihsanul1234@users.noreply.github.com>
2023-10-02 19:09:04 +0530

    Add files via upload

---
+++ java2.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.*;
+class java2
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=a%2;
+if(b==0)
+{
+System.out.println("Even");
+}
+else
+{
+System.out.println("odd");
+}
+
+}
+}
+
---
+++ java3.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+import java.util.*;
+class java#
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner s=new Scanner(System.in);
+a=s.nextInt()
